a7d56aae6b0272cf46eb811055aab1e9675bea0c
prjwl16 <prajumore76@gmail.com>
2023-07-17 01:53:57 +0530

    design config added to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,30 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors:{
+        black: "#0B0D17",
+        lightPurpule:"#D0D6F9",
+        white:"#FFFFFF"
+      },
+      fontFamily: {
+        barlowCondensed: ["barlow-condensed", "sans-serif"],
+        bellafair: ["bellafair", "sans-serif"],
+      },
+      fontSize: {
+        "h1":"150px",
+        "h2":"100px",
+        "h3":"56px",
+        "h4":"32px",
+        "h5":"28px",
+        "s2":"14px",
+        "nav":"16px",
+        "body":"18px"
+      },
+      spacing: {
+        "xs":"4.75ch",
+        "s":"2.35ch",
+        "m":"3.7ch",
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
